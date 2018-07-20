d13e003653e596a127a1c460e556b806ceea9e20
Eric M Todd <ericmtodd@Erics-MacBook-Air.local>
2018-07-20 09:58:30 -0600

    Cleaned up code template

--- server.js
+++ server.js
@@ -1,15 +1,3 @@
 console.log("server.js is running...");
 
 
-// Required npm modules
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
-const methodOverride = require("method-override");
-
-
-// Required database
-require("./db/db");
-
-// Required middleware
-app.use(bodyParser.)
