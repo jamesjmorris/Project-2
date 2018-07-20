c478de5538214e538f666ef1163da1098e0b5d3e
Eric M Todd <ericmtodd@Erics-MacBook-Air.local>
2018-07-20 09:56:04 -0600

    Added express-session

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
 console.log("server.js is running...");
 
 
+// Required npm modules
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+
+
+// Required database
+require("./db/db");
+
+// Required middleware
+app.use(bodyParser.)
