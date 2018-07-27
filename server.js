85f189ff37cb402deb03b63256b0c450d146830f
James M <saintjames@MacBook-Pro-2.local>
2018-07-27 10:05:37 -0600

    heroku test 2

--- server.js
+++ server.js
@@ -55,6 +55,7 @@ app.use((req, res, next) => {
 // This allows the user to remain logged in after restarting the server.
 const store = new MongoDBStore({
   const mongoUri = process.env.MONGODB_URI ||"mongodb://localhost:27017/gladiator",
+  mongoose.connect(mongoUri);
   // uri: 'mongodb://localhost:27017/connect_mongodb_session_test',
   collection: 'mySessions'
 });
